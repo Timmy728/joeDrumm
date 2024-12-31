53b57268169964200c03b14c2f7f66684d8486b9
Timmy728 <jdrumm500@hotmail.com>
2024-12-31 16:51:42 +0000

    HTML App JavaScript.js

---
+++ Task/libs/Js/App JavaScript.js
@@ -0,0 +1,47 @@
+// Street Name LookUp API
+document.getElementById("btnStreetLookUp").addEventListener("click", function() {
+    const streetName = document.getElementById("streetName").value;
+
+    fetch("libs/php/streetNameLookUp.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ streetName: streetName }),
+    })
+    then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      if (result.status.name === "ok") {
+        document.getElementById("txtStreetResult").innerHTML =
+          result.data[0].streetDetails || "No details found."; // Example field
+      }
+    })
+    .catch((error) => console.error("error:", error));
+});
+
+// Weather API Functionality
+document.getElementById("btnWeather").addEventListener("click", function() {
+    const location = document.getElementById("location").value;
+
+    fetch("libs/php/weather.php", {
+        method: "POST",
+        headers {
+        "Content-Type": "application/json",
+},
+ body: JSON.stringify({ location: location}),
+})
+    .then((response) => response.json())
+    .then((result) => {
+        console.log(result);
+        if (result.status.name === "ok") {
+            document.getElementById("txtWeatherResult").innerHTML =
+                result.data[0].weatherInfo || "weather information not found."; // Example
+        }
+    })
+    .catch((error) => console.log("Error:", error));
+});
+
+// Country Code LookUp API Functionality
+
+

9c57be80d49452ced0a746732141ca1b2dc88866
Timmy728 <jdrumm500@hotmail.com>
2025-01-11 20:22:54 +0000

    getWeatherForecast.php

--- Project1/Php/getWeatherForecast.php
+++ Project1/Php/getWeatherForecast.php
@@ -1,1 +1,40 @@
+<?php
+function fetchWeatherForecast($lat, $lon, $cnt = 16) {
+    $apiKey = '40239c7b19a7290e280d24cc348eb7f6'; 
+    $apiUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=$lat&lon=$lon&cnt=$cnt&units=metric&appid=$apiKey";
 
+    // Make API call
+    $response = file_get_contents($apiUrl);
+
+    if ($response === false) {
+        return json_encode(['error' => 'Unable to fetch forecast data.']);
+    }
+
+    $forecastData = json_decode($response, true);
+
+    // Simplify data for easier use
+    $dailyForecasts = [];
+    foreach ($forecastData['list'] as $day) {
+        $dailyForecasts[] = [
+            'date' => date('Y-m-d', $day['dt']),
+            'temperature' => [
+                'min' => $day['temp']['min'] . '°C',
+                'max' => $day['temp']['max'] . '°C',
+            ],
+            'description' => $day['weather'][0]['description'],
+            'icon' => "https://openweathermap.org/img/wn/" . $day['weather'][0]['icon'] . "@2x.png",
+        ];
+    }
+    return json_encode($dailyForecasts);
+}
+
+// Handle incoming request
+if (isset($_GET['lat']) && isset($_GET['lon'])) {
+    $lat = htmlspecialchars($_GET['lat']);
+    $lon = htmlspecialchars($_GET['lon']);
+    $cnt = isset($_GET['cnt']) ? intval($_GET['cnt']) : 16;
+    echo fetchWeatherForecast($lat, $lon, $cnt);
+} else {
+    echo json_encode(['error' => 'Latitude and longitude parameters are required.']);
+}
+?>

aee771ade5f57b86a5f86934cba875922d2a9904
Timmy728 <jdrumm500@hotmail.com>
2025-01-18 18:38:31 +0000

    script.js

--- Project1/Js/script.js
+++ Project1/Js/script.js
@@ -15,7 +15,7 @@ var adminCityClusterGroup = L.markerClusterGroup({
     maxClusterRadius: 25
 });
 
-// Set up the map with default tile layers
+// Tile layers
 var streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'

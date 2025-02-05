5b156c247e98c2d7b66a58497a181c4cc4d074cb
Timmy728 <jdrumm500@hotmail.com>
2025-02-05 19:48:33 +0000

    Currency.php

--- Project1/Php/Currency.php
+++ Project1/Php/Currency.php
@@ -1,31 +1,40 @@
 <?php
-
 // remove for production
-
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 $executionStartTime = microtime(true);
 
-$url= "https://openexchangerates.org/api/currencies.json";
+// API URLs
+$currenciesUrl = "https://openexchangerates.org/api/currencies.json";
+$exchangeRateUrl = "https://openexchangerates.org/api/latest.json?app_id=39e1723bbec3413181d6bbe3c3d1d3c8";
 
+// Fetch Currency Names
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL,$url);
-
-$result=curl_exec($ch);
+curl_setopt($ch, CURLOPT_URL, $currenciesUrl);
+$currenciesResult = curl_exec($ch);
 curl_close($ch);
 
-$decode = json_decode($result,true);  
+$currenciesData = json_decode($currenciesResult, true);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $exchangeRateUrl);
+$exchangeRateResult = curl_exec($ch);
+curl_close($ch);
+
+$exchangeRateData = json_decode($exchangeRateResult, true);
+
 $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
-
 $output['status']['description'] = "success";
 $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output['data'] = $decode;
+$output['currencies'] = $currenciesData;
+$output['exchangeRates'] = $exchangeRateData['rates'];
 
 header('Content-Type: application/json; charset=UTF-8');
-
 echo json_encode($output);
+
 ?>

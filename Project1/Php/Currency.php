0996b45c414250a31a35995c72abb3748774d4e1
Timmy728 <jdrumm500@hotmail.com>
2025-02-05 18:17:17 +0000

    Currency.php

--- Project1/Php/Currency.php
+++ Project1/Php/Currency.php
@@ -1,31 +1,31 @@
-<?php 
+<?php
 
-$url = "https://openexchangerates.org/api/currencies.json";
+// remove for production
 
-//Initialize cURL
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+$executionStartTime = microtime(true);
+
+$url= "https://openexchangerates.org/api/currencies.json";
+
 $ch = curl_init();
-curl_setopt($ch, CURLOP_URL, $url);
-curl_setopt($ch, CURLOP_RETURNTRANSER, true);
 
-//Execute API request
-£response = curl_exec($ch);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$url);
 
-//Checks for cURL errors
-if (curl_errno($ch)) {
-  echo 'cURL Error: ' curl_error($ch);
-  exit;
-}
-
+$result=curl_exec($ch);
 curl_close($ch);
 
-$data = json_decode($response, true);
+$decode = json_decode($result,true);  
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
 
-//This displays the currencies
-if ($data) {
-  echo "Supported Currencies:/n;
-  foreach ($data as $code => $name) {
-  echo "$code: $name/n;
-} else {
-  echo "Failed to retrieve currencies./n";
-}
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+$output['data'] = $decode;
+
+header('Content-Type: application/json; charset=UTF-8');
+
+echo json_encode($output);
 ?>

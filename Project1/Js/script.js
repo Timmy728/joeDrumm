218358fbd2a41071f10f157e78cb5495a91cd1fa
Timmy728 <jdrumm500@hotmail.com>
2025-02-23 20:49:39 +0000

    script.js

--- Project1/Js/script.js
+++ Project1/Js/script.js
@@ -12,23 +12,66 @@ let selectedCountryISO2;
 let countryBordersData;
 
 $(document).ready(function () {
+    // Initialize the map
     map = L.map('map').setView([20, 0], 2);
 
-    let streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
-        attribution: "Tiles &copy; Esri"
-    });
-    
-    let satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
-        attribution: "Tiles &copy; Esri"
+    // Add tile layers
+    var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+        attribution: "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
     });
 
-    let basemaps = {
+    var satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
+        attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+    });
+
+    var basemaps = {
         "Streets": streets,
         "Satellite": satellite
     };
 
-    streets.addTo(map);
     L.control.layers(basemaps).addTo(map);
+    streets.addTo(map);
+
+    // Add 10 EasyButtons for each modal
+    L.easyButton('fa-flag', function () {
+        $('#countryInfoModal').modal('show');
+    }).addTo(map);
+
+    L.easyButton('fa-city', function () {
+        $('#capitalCityModal').modal('show');
+    }).addTo(map);
+
+    L.easyButton('fa-users', function () {
+        $('#populationModal').modal('show');
+    }).addTo(map);
+
+    L.easyButton('fa-money-bill', function () {
+        $('#currencyModal').modal('show');
+    }).addTo(map);
+
+    L.easyButton('fa-exchange-alt', function () {
+        $('#exchangeRateModal').modal('show');
+    }).addTo(map);
+
+    L.easyButton('fa-cloud-sun', function () {
+        $('#weatherModal').modal('show');
+    }).addTo(map);
+
+    L.easyButton('fa-calendar-day', function () {
+        $('#forecastModal').modal('show');
+    }).addTo(map);
+
+    L.easyButton('fa-wikipedia-w', function () {
+        $('#wikipediaModal').modal('show');
+    }).addTo(map);
+
+    L.easyButton('fa-clock', function () {
+        $('#timezoneModal').modal('show');
+    }).addTo(map);
+
+    L.easyButton('fa-globe', function () {
+        $('#earthquakeModal').modal('show');
+    }).addTo(map);
 
     // Populate countries dropdown
     $.ajax({
@@ -50,6 +93,7 @@ $(document).ready(function () {
         }
     });
 
+    // On country selection
     $('#countrySelect').change(function () {
         const iso2 = $(this).val();
         if (iso2) {
@@ -58,7 +102,7 @@ $(document).ready(function () {
     });
 
     function fetchAllCountryData(iso2) {
-        displayCountryName(iso2);
+        displayCountryInfo(iso2);
         displayCapitalCity(iso2);
         displayPopulation(iso2);
         displayCurrency(iso2);
@@ -71,7 +115,30 @@ $(document).ready(function () {
         updateCountryBorders(iso2);
     }
 
-    function displayCountryName(iso2) {
+    function updateCountryBorders(iso2) {
+        if (bordersLayer) {
+            map.removeLayer(bordersLayer);
+        }
+        $.getJSON('Data/countryBorders.geo.json', function (data) {
+            const country = data.features.find(
+                feature => feature.properties.iso_a2 === iso2
+            );
+            if (country) {
+                bordersLayer = L.geoJSON(country, {
+                    style: {
+                        color: 'blue',
+                        weight: 2,
+                        fillColor: 'orange',
+                        fillOpacity: 0.3
+                    }
+                }).addTo(map);
+                map.fitBounds(bordersLayer.getBounds());
+            }
+        });
+    }
+
+    // Display country information
+    function displayCountryInfo(iso2) {
         $.get('Php/countryName.Php', { iso2: iso2 }, function (data) {
             $('#countryNames').text(data.name);
         }, 'json');
@@ -85,88 +152,57 @@ $(document).ready(function () {
 
     function displayPopulation(iso2) {
         $.get('Php/Population.Php', { countryCode: iso2 }, function (data) {
-            console.log('Population data:', data);
-            $('#population').text(data.population || 'Data not available');
+            $('#population').text(data.population);
         }, 'json');
     }
 
     function displayCurrency(iso2) {
         $.get('Php/Currency.Php', { iso2: iso2 }, function (data) {
-            console.log('Currency data:', data);
-            $('#currencyName').text(data.name || 'N/A');
-            $('#currencySymbol').text(data.symbol || 'N/A');
+            $('#currencyName').text(data.name);
         }, 'json');
     }
 
     function displayExchangeRate(iso2) {
         $.get('Php/latestExchangeRate.php', { iso2: iso2 }, function (data) {
-            console.log('Exchange rate:', data);
             $('#txtCurrencyRate').text(`1 USD = ${data.exchangeRate} ${data.currencyCode}`);
         }, 'json');
     }
 
     function displayWeather(iso2) {
         $.get('Php/getWeather.Php', { iso2: iso2 }, function (data) {
-            console.log('Weather data:', data);
-            $('#tempToday').text(data.temperature || 'N/A');
-            $('#conditionsToday').text(data.description || 'N/A');
-            $('#weatherImg').html(`<img src="${data.icon}" alt="Weather Icon">`);
+            $('#tempToday').text(data.temperature);
+            $('#conditionsToday').text(data.description);
         }, 'json');
     }
 
     function displayWeatherForecast(iso2) {
         $.get('Php/getWeatherForecast.Php', { location: iso2 }, function (data) {
-            console.log('Forecast data:', data);
-            let forecastHtml = '';
+            $('#forecastInfo').html('');
             data.forEach(forecast => {
-                forecastHtml += `<p>${forecast.date}: ${forecast.min_temp}°C - ${forecast.max_temp}°C</p>`;
+                $('#forecastInfo').append(`<p>${forecast.date}: ${forecast.min_temp}°C - ${forecast.max_temp}°C</p>`);
             });
-            $('#forecastInfo').html(forecastHtml);
         }, 'json');
     }
 
     function displayWikipediaInfo(iso2) {
         $.get('Php/wikipediaSearch.Php', { query: iso2 }, function (data) {
-            console.log('Wikipedia data:', data);
             $('#wikiLink').attr('href', data.url).text(`View ${data.title} on Wikipedia`);
         }, 'json');
     }
 
     function displayTimezone(iso2) {
         $.get('Php/Timezone.Php', { iso2: iso2 }, function (data) {
-            console.log('Timezone data:', data);
             $('#timezone').text(data.timezone);
         }, 'json');
     }
 
     function displayEarthquakeData(iso2) {
         $.get('Php/earthQuakes.Php', { country: iso2 }, function (data) {
-            console.log('Earthquake data:', data);
             let earthquakeHtml = '';
             data.earthquakes.forEach(quake => {
                 earthquakeHtml += `<p>📍 Magnitude: ${quake.magnitude} | Depth: ${quake.depth}km | Location: (${quake.lat}, ${quake.lng}) | Time: ${quake.datetime}</p>`;
             });
             $('#earthquakeList').html(earthquakeHtml);
         }, 'json');
-    }
-
-    function updateCountryBorders(iso2) {
-        if (bordersLayer) {
-            map.removeLayer(bordersLayer);
-        }
-        $.getJSON('Data/countryBorders.geo.json', function (data) {
-            const country = data.features.find(feature => feature.properties.iso_a2 === iso2);
-            if (country) {
-                bordersLayer = L.geoJSON(country, {
-                    style: {
-                        color: 'blue',
-                        weight: 2,
-                        fillColor: 'orange',
-                        fillOpacity: 0.3
-                    }
-                }).addTo(map);
-                map.fitBounds(bordersLayer.getBounds());
-            }
-        });
     }
 });

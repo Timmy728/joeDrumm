94908695ba3bcdab697043eb83d4483a58700e25
Timmy728 <jdrumm500@hotmail.com>
2025-02-23 14:59:16 +0000

    script.js

--- Project1/Js/script.js
+++ Project1/Js/script.js
@@ -11,39 +11,31 @@ let bordersLayer;
 let selectedCountryISO2;
 let countryBordersData;
 
-var map;
-
 $(document).ready(function () {
-  
-  map = L.map("map", {
-    layers: [streets]
-  }).setView([54.5, -4], 6);
+    // Tile layers
+    var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+        attribution: "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
+    });
 
-layerControl = L.control.layers(basemaps).addTo(map);
-
-infoBtn.addTo(map);
-
-})
-
-    // Add tile layer
-var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
-    attribution: "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
-  }
-);
     var satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
-    attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-  }
-);
+        attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+    });
+
     var basemaps = {
-  "Streets": streets,
-  "Satellite": satellite
-};
-    var infoBtn = L.easyButton("fa-info fa-xl", function (btn, map) {
-  $("#exampleModal").modal("show");
-});
+        "Streets": streets,
+        "Satellite": satellite
+    };
+
+    // Initialize map with default view
+    map = L.map("map", {
+        layers: [streets]
+    }).setView([54.5, -4], 6);
+
+    // Add layer control for basemaps
+    L.control.layers(basemaps).addTo(map);
 
     // Add EasyButton for displaying country info
-    L.easyButton('fa-info', function () {
+    L.easyButton('fa-info fa-xl', function () {
         $('#countryInfoModal').modal('show');
     }).addTo(map);
 
@@ -88,15 +80,6 @@ var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/
         updateCountryBorders(iso2);
     }
 
-    function fetchCoordinatesAndDisplayWeather(iso2) {
-        $.get('https://restcountries.com/v3.1/alpha/' + iso2, function (data) {
-            if (data && data[0] && data[0].latlng) {
-                const [lat, lon] = data[0].latlng;
-                displayWeather(lat, lon);
-            }
-        }, 'json');
-    }
-
     function updateCountryBorders(iso2) {
         if (bordersLayer) {
             map.removeLayer(bordersLayer);
@@ -117,83 +100,5 @@ var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/
                 map.fitBounds(bordersLayer.getBounds());
             }
         });
-    }
-
-    function displayCountryInfo(iso2) {
-        $.get('Php/countryName.Php', { iso2: iso2 }, function (data) {
-            $('#countryNames').text(data.name);
-            $('#capitalCity').text(data.capital);
-            $('#population').text(data.population);
-        }, 'json');
-    }
-
-    function displayPopulation(iso2) {
-        $.get('Php/Population.Php', { countryCode: iso2 }, function (data) {
-            $('#population').text(data.population);
-        }, 'json');
-    }
-
-    function displayWeather(lat, lon) {
-        $.get('Php/getWeather.Php', { lat: lat, lon: lon }, function (data) {
-            $('#tempToday').text(data.temperature);
-            $('#conditionsToday').text(data.description);
-            $('#weatherImg').html(`<img src="${data.icon}" alt="Weather Icon">`);
-        }, 'json');
-    }
-
-    function displayTimezone(iso2) {
-        $.get('Php/Timezone.Php', { iso2: iso2 }, function (data) {
-            $('#timezone').text(data.timezone);
-        }, 'json');
-    }
-
-    function displayCurrency(iso2) {
-        $.get('Php/Currency.Php', { iso2: iso2 }, function (data) {
-            if (data && data.currencies && data.currencies.length > 0) {
-                let currency = data.currencies[0];
-                $('#currencyName').text(`${currency.name} (${currency.code}) - ${currency.symbol}`);
-            } else {
-                $('#currencyName').text('Currency not available');
-            }
-        }, 'json');
-    }
-
-    function displayExchangeRate(iso2) {
-        $.get('Php/latestExchangeRate.php', { iso2: iso2 }, function (data) {
-            $('#txtCurrencyRate').text(`1 USD = ${data.exchangeRate} ${data.currencyCode}`);
-        }, 'json');
-    }
-
-    function displayWikipediaInfo(iso2) {
-        $.get('https://restcountries.com/v3.1/alpha/' + iso2, function (data) {
-            if (data && data[0]) {
-                const countryName = data[0].name.common;
-                $.get('Php/wikipediaSearch.Php', { query: countryName }, function (wikiData) {
-                    $('#wikiLink').attr('href', wikiData.url).text(`View ${wikiData.title} on Wikipedia`);
-                }, 'json');
-            }
-        }, 'json');
-    }
-
-    function displayEarthquakeData(iso2) {
-        $.get('Php/earthQuakes.Php', { country: iso2 }, function (data) {
-            let earthquakeHtml = '';
-            data.earthquakes.forEach(quake => {
-                earthquakeHtml += `<p>📍 Magnitude: ${quake.magnitude} | Depth: ${quake.depth}km | Location: (${quake.lat}, ${quake.lng}) | Time: ${quake.datetime}</p>`;
-            });
-            $('#earthquakeList').html(earthquakeHtml);
-        }, 'json');
-    }
-
-    function displayWeatherForecast(iso2) {
-        $.get('Php/getWeatherForecast.Php', { location: iso2 }, function (data) {
-            let forecastHtml = '<h4>10-Day Weather Forecast:</h4>';
-            data.forEach(forecast => {
-                let minTemp = forecast.min_temp !== null ? forecast.min_temp + '°C' : 'No Data';
-                let maxTemp = forecast.max_temp !== null ? forecast.max_temp + '°C' : 'No Data';
-                forecastHtml += `<p>${forecast.date}: ${minTemp} - ${maxTemp}</p>`;
-            });
-            $('#forecastInfo').html(forecastHtml);
-        }, 'json');
     }
 });

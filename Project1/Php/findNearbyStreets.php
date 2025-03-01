01c5b84c0440f37d4b280a0aa0e9bc46581cb26e
Timmy728 <jdrumm500@hotmail.com>
2025-03-01 17:06:52 +0000

    findNearbyStreets.php

---
+++ Project1/Php/findNearbyStreets.php
@@ -0,0 +1,34 @@
+<?php 
+header("Content-Type: application/json");
+
+$apiKey = "joedrumm12";
+
+// Check if lat & lon are provided
+if (!isset($_GET['lat']) || !isset($_GET['lon'])) {
+    echo json_encode(["error" => "Latitude and Longitude required"]);
+    exit;
+}
+
+$lat = $_GET['lat'];
+$lon = $_GET['lon'];
+
+$url = "http://api.geonames.org/findNearbyStreetsJSON?lat=$lat&lng=$lon&username=$apiKey";
+
+$curl = curl_init();
+curl_setopt_array($curl, [
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+]);
+
+$response = curl_exec($curl);
+curl_close($curl);
+
+$data = json_decode($response, true);
+
+if (isset($data['streetSegment'])) {
+    echo json_encode($data['streetSegment']);
+} else {
+    echo json_encode(["error" => "No nearby streets found"]);
+}
+?>

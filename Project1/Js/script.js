3596a8d38c868d2d41c63a10e0008e4e05f088c3
Timmy728 <jdrumm500@hotmail.com>
2025-03-17 22:27:49 +0000

    script.js

--- Project1/Js/script.js
+++ Project1/Js/script.js
@@ -20,9 +20,12 @@ $(document).ready(function () {
         minZoom: 2
     }).setView([20, 0], 2);
 
-    // Initialize marker cluster group
-    earthquakeLayer = L.markerClusterGroup();
-    map.addLayer(earthquakeLayer);
+   earthquakeLayer = L.markerClusterGroup({
+        showCoverageOnHover: false,
+        zoomToBoundsOnClick: true,
+        spiderfyOnMaxZoom: true,
+        removeOutsideVisibleBounds: true
+    });
 
     // Add tile layers
     var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
